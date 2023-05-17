09902758d587d1638d3947501c1b35e27894fe87
Maxim <max2morozov@gmail.com>
2023-05-17 18:05:02 +0300

    update str_func.py + hello

--- str_func.py
+++ str_func.py
@@ -1,6 +1,7 @@
 def stroka_up():
     """ Первая ф-я, которая принимает на вход строку и возвращает ее со всеми заглавными
     """
+    print("Hello")
     s = input()
     print(s.upper())
     return
@@ -8,6 +9,7 @@ def stroka_up():
 def stroka_title():
     """ функция, которая принимает на вход строку и возвращает ее заглавной буквой.
     """
+    print("Hello")
     d = input()
     print(d.title())
     return
